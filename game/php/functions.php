aa27f632250fa747b575fe7111dd2a140d66ceb6
Patrick Obermueller <pobermueller.mmt-b2015@fh-salzburg.ac.at>
2018-05-27 05:05:30 +0200

    PDO dependencies

--- game/php/functions.php
+++ game/php/functions.php
@@ -14,6 +14,7 @@ try {
 			$db["pass"],
 			ltrim($db["path"], "/")
 		));
+
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
