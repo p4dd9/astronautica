8d0ca70df03601637a9cc3210d08e283bc084a6b
Patrick Obermueller <pobermueller.mmt-b2015@fh-salzburg.ac.at>
2018-06-15 12:47:24 +0200

    Prompt add to homescreen button

--- game/sw.js
+++ game/sw.js
@@ -3,6 +3,17 @@ if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/sw.js').then(function(registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
+
+            let installPromptEvent;
+
+            window.addEventListener('beforeinstallprompt', (event) => {
+                // Prevent Chrome <= 67 from automatically showing the prompt
+                event.preventDefault();
+                // Stash the event so it can be triggered later.
+                installPromptEvent = event;
+                // Update the install UI to notify the user app can be installed
+                event.prompt()
+            });
         }, function(err) {
             // registration failed :(
             console.log('ServiceWorker registration failed: ', err);
