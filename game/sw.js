7c7c722db7ddc1b3ae0c317d85014aab61e0724c
Patrick Obermueller <pobermueller.mmt-b2015@fh-salzburg.ac.at>
2018-06-15 13:31:24 +0200

    Clean comments

--- game/sw.js
+++ game/sw.js
@@ -1,10 +1,8 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
         navigator.serviceWorker.register('/sw.js').then(function(registration) {
-            // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function(err) {
-            // registration failed :(
             console.log('ServiceWorker registration failed: ', err);
         });
     });
@@ -12,13 +10,8 @@ if ('serviceWorker' in navigator) {
     let installPromptEvent;
 
     window.addEventListener('beforeinstallprompt', (event) => {
-        // Prevent Chrome <= 67 from automatically showing the prompt
         event.preventDefault()
-        // Stash the event so it can be triggered later.
         installPromptEvent = event
-        console.log("beforeinstallprompt - Event")
-        console.log(event)
-        // Update the install UI to notify the user app can be installed
         installPromptEvent.prompt()
     });
 }
